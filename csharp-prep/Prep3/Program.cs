cdec28565d54a27f3a161d89b73fef3aaf0e7b67
teddy2j <teddy2jem@gmail.com>
2023-09-18 04:44:32 -0500

    first week

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //for part 1 
+        //Console.WriteLine("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine())
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -10;
+
+        while ( guess != magicNumber)
+        {
+          Console.Write("What is your guess? ");
+          guess = int.Parse(Console.ReadLine());
+
+          if  (magicNumber > guess) 
+          {
+            Console.WriteLine("Higuer");
+          } 
+          else if (magicNumber< guess)
+          {
+            Console.WriteLine("Lower");
+          }
+          else
+          {
+            Console.WriteLine("You guessed it!");
+          }
+        }
     }
 }
