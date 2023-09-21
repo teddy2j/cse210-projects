fda6a56d447961d2e7b639bd22f329968fb4521b
teddy2j <teddy2jem@gmail.com>
2023-09-21 15:24:17 -0500

    Abstraction learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2017;
+        job1._endYear= 2022;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "CEO";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+
+
+        Resume resume = new Resume();
+        resume._name = "Allison Rosa";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
     }
 }
